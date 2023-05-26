7dc92c591f06e5693d23d875c2e2df7671afd143
PebblesProgramming <milan0709@hotmail.nl>
2023-05-26 22:56:48 +0200

    testing out UI style

---
+++ applicatie/components/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <h4>Contact</h4>
+    <p>Gelre-Check-In <br>
+       Karperstraat 728 <br>
+       8374 AZ Rotterdam <br> 
+    </p>
+    <br>
+    <h4>Copyright</h4>
+    <p>de Gelre-Check in-service, inclusief alle content die wordt geleverd via de Gelre-Check in-service, <br>
+       wordt beschermd door wetten en verdragen op het gebied van auteursrecht, handelsgeheimen of andere intellectuele-eigendomsrechten.</p>
+       <br>
+    <p> <a href="Privacy-Verklaring.php">Privacy Verklaring</a> </p>
+</footer>
