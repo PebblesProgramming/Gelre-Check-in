4a28c06b9ba7295cb8dd0d8ee29ef2695a05ee26
PebblesProgramming <milan0709@hotmail.nl>
2023-06-06 21:26:31 +0200

    UI Updates for Home page + start for other pages

--- applicatie/index.php
+++ applicatie/index.php
@@ -12,41 +12,68 @@
 <body>
     <div class="container">
         <div class="navbar">
-            <img src="images/actie.jpg" class="logo">
+            <img src="favicon.ico" class="logo"> 
+            <h4>Welkom insgebruiker </h4>
             <nav>
                 <ul id="menuList">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">CheckIn</a></li>
-                    <li><a href="">Vluchten</a></li>
-                    <li><a href="">Profiel</a></li>
+                    <li><input type="text" placeholder="Zoek vlucht..."></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="screens/vluchten.php">Vluchten</a></li>
+                    <li><a href="screens/inchecken.php">Inchecken</a></li>
+                    <li><a href="screens/profile.php">Profiel</a></li>
                     <li><a href="">Uitloggen</a></li> <!-- uitloggen? -->
                 </ul>
             </nav>
-            <img src="images/adventure.jpg" class="menu-icon"> <!-- figure thi out without using js -->
+            <img src="images/menu.png" class="menu-icon"> <!-- figure thi out without using js -->
         </div>
 
         <div class="row">
             <div class="col-1">
                 <h2> Gelre Check In</h2>
-                <h3> Nu Inchecker</h3>
-                <p>Overal te doen</p>
-                <h4>$3400</h4>
-                <button type="button">Boek Nu</button>
+                <h3> Wilt u goed aan de vakantie beginnen?</h3>
+                <p>Check nu in!</p>
+                <h4>max €90 extra!</h4>
+                <button type="button">Check In</button>
             </div>
             <div class="col-2">
-                <img src="images/GelreCheckIn.jpg" class="checkin">
+                <img src="images/checkin.png" class="checkin">
                 <div class="color-box"></div> <!-- Awesome background effect, look for better photo -->
                 <div class="add-btn"> <!-- try and find an awesome implimintation for this -->
-                    <img src="images/drama.jpg">
-                    <p>Meer acties</p>
+                    <img src="images/add.png">
+                    <p>Check In!</p> <!-- add flights for employee UI-->
                 </div>
             </div>
         </div>
+        
+        
+         <h1 class="h1flights">Bekijk Vluchten</h1> 
+         <button type="button" class="buttonFlights">Bekijk alles</button> 
+            <br>
+            <br>
+            <br>
+        <div class="wrapper">
+            <div class="item">box-1</div>
+            <div class="item">box-2</div>
+            <div class="item">box-3</div>
+            <div class="item">box-4</div>
+            <div class="item">box-5</div>
+            <div class="item">box-6</div>
+            <div class="item">box-7</div>
+            <div class="item">box-8</div>
+            <div class="item">box-9</div>
+            <div class="item">box-10</div>
+            <div class="item">box-11</div>
+            <div class="item">box-12</div>
+        </div>
+    
+
+        
+        
 
         <div class="social-links"> <!--change these to socials -->
-            <img src="images/crime.jpg">
-            <img src="images/crime.jpg">
-            <img src="images/crime.jpg">
+            <img src="images/fb.png">
+            <img src="images/ig.png">
+            <img src="images/tw.png">
         </div>
     </div>
 </body>
